ab97c25a0231169582f824acd4eb55144b146f9c
Vivek <3vivekb@gmail.com>
2013-10-23 23:40:49 -0700

    umm, 128 again?
    I dont know

--- comparecsv.R
+++ comparecsv.R
@@ -1,16 +1,40 @@
+library(plyr)
 wat5 <- read.table("/Volumes/Windows/optix/submission/watsonsubmission5.txt", quote="\"")
 str(watsonsubmission5)
+#This was random forest
 
-wat3 <- read.table("/Volumes/Windows/optix/submission/watsonsubmission3.txt", quote="\"")
+#wat3 <- read.table("/Volumes/Windows/optix/submission/watsonsubmission3.txt", quote="\"")
 
-wat4 <- read.table("/Volumes/Windows/optix/submission/watsonsubmission4.txt", quote="\"")
+wat12 <- read.table("/Volumes/Windows/optix/submission/watsonsubmission12.txt", quote="\"")
 
 
 nrow(wat5)
 nrow(wat4)
 wat<- merge(wat5, wat4)
 #http://hosho.ees.hokudai.ac.jp/~kubo/Rdoc/library/plyr/html/join.html
-join(wat4, wat5, type = "")
+
+watfull<-join(wat4, wat12, type = "full")
+watleft<-join(wat4, wat12, type = "left")
+watright<-join(wat4, wat12, type = "right")
+watinner<-join(wat4, wat12, type = "inner")
+
+#watfull - a combo of RF and Logistic Reg
+write.table(watfull, file = "watsonsubmission13.txt", row.names = FALSE, col.names = FALSE)
+nrow(watfull)
+#length = 275
+#Score = 127
+
+#watleft - a combo of RF and Logistic Reg - sub 5 and sub 12
+write.table(watleft, file = "watsonsubmission14.txt", row.names = FALSE, col.names = FALSE)
+nrow(watleft)
+#length = 210
+#Score = 108
+
+#watright - a combo of RF and Logistic Reg - sub 5 and sub 12
+write.table(watright, file = "watsonsubmission15.txt", row.names = FALSE, col.names = FALSE)
+nrow(watright)
+#length = 272
+#Score = 128
 
 str(wat)
 wat
